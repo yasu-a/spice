02984911b474b79d8cc6e77db1930175b66da05e
yasu-a <yasuhiro.gtgt@gmail.com>
2023-10-26 04:06:07 +0900

    nonlinear analysis by spice.optimize.root

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def main():
         )
     )
 
-    with open('sample3.cir', 'r') as f:
+    with open('sample1.cir', 'r') as f:
         netlist = NetList.from_string(
             source=f.read(),
             class_set=class_set
@@ -82,59 +82,63 @@ def main():
     pprint(rec)
     ctx = dict(var_record=rec, edge_currents=ohm_dct, used_edge_currents={})
     pprint(ctx)
-    total_dict = {}
+    total_dict: dict[object, str] = {}
     for a, (b, c) in dct.items():
         total_dict[a] = b.to_python(ctx) + '-' + c.to_python(ctx)
     for a, (b, c) in ctx['used_edge_currents'].items():
         if a in dct:
             continue
         total_dict[a] = b.to_python(ctx) + '-' + c.to_python(ctx)
+    print('total_dict')
     pprint(total_dict)
-    xs = {x for v in total_dict.values() for x in re.findall(r'x__\w_[\w\d]+', v)}
-    pprint(xs)
-    assert len(total_dict) == len(xs), (len(total_dict), len(xs))
+    xs_name: list[str] = sorted({x for v in total_dict.values() for x in re.findall(r'x__\w_[\w\d]+', v)})
+    print('xs_name')
+    pprint(xs_name)
+    assert len(total_dict) == len(xs_name), (len(total_dict), len(xs_name))
 
-    k_sorted = sorted(total_dict, key=repr)
+    def expr_to_funcs(var_names: list[str], exprs: list[str]):
+        exprs_name_converted: list[str] = []
 
-    funcs = [f'f_dct[{i}] = lambda {", ".join(xs)}: {total_dict[k]}'
-             for i, k in enumerate(k_sorted)]
-    pprint(funcs)
-    funcs = [compile(f, f'__eq_{i}__', mode='exec') for i, f in enumerate(funcs)]
-    # pprint(funcs)
-    f_dct = {}
-    for f in funcs:
-        exec(f, dict(f_dct=f_dct))
-    fs = [f_dct[i] for i, k in enumerate(k_sorted)]
+        for expr in exprs:
+            for i, var_name in enumerate(var_names):
+                expr = expr.replace(var_name, f'_x[{i}]')
+            exprs_name_converted.append(expr)
 
-    DELTA = 1e-12
+        f_lst_expr = '[' + ', '.join(expr for expr in exprs_name_converted) + ']'
+        f_expr = f'_f_placeholder[0] = lambda _x: {f_lst_expr}'
+        f_expr_compiled = compile(f_expr, f'__eqs__', mode='exec')
+        _f_placeholder = {}
+        exec(f_expr_compiled, dict(_f_placeholder=_f_placeholder))
+        f = _f_placeholder[0]
+        return f
 
-    def df_of_f(f, xi):
-        def df(x0):
-            xl = np.array(x0)
-            xl[xi] -= DELTA / 2
-            xh = np.array(x0)
-            xh[xi] += DELTA / 2
-            return (f(*xh) - f(*xl)) / DELTA
+    func = expr_to_funcs(xs_name, list(total_dict.values()))
+    print(func)
 
-        return df
+    x0 = np.zeros(len(xs_name))
+    print(func(x0))
 
-    dfs = [[df_of_f(fs[i], j) for j in range(len(fs))] for i in range(len(fs))]
-    pprint(fs)
-    pprint(dfs)
+    from scipy.optimize import root
 
-    x0 = np.zeros(len(xs))
-    for _ in range(100):
-        f_x0 = np.array([fs[i](*x0) for i, k in enumerate(k_sorted)])
-        print(f_x0)
-        jacob = np.array([[dfs[i][j](x0) for j in range(len(fs))] for i in range(len(fs))])
-        print(jacob)
+    y = root(func, x0)
+    print(y)
+    pprint(dict(zip(map('{:10s}'.format, xs_name), map('{:12.8f}'.format, y['x']))))
 
-        import gauss
+    x0_min = np.array([7 if name.startswith('x__i_') else 0.01 for name in xs_name])
+    x0_max = -x0_min
 
-        r = gauss.solve(jacob, f_x0[:, None])
-        print(r)
-        x0 = x0 - r
-        print(x0)
+    print(np.mgrid[tuple(for v_min, v_max in zip(x0_min, x0_max))])
+
+    from sklearn.decomposition import PCA
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.pipeline import Pipeline
+
+    pl = Pipeline([
+        ('std', StandardScaler())
+        ('pca', PCA(n_components=2))
+    ])
+
+    pl.fit()
 
 
 if __name__ == '__main__':
